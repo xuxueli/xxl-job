ae758eb8a96183a42643117feacc1556e29d7e95
xueli.xue <xueli.xue@dianping.com>
2016-09-06 15:46:33 +0800

    doc 更新

--- xxl-job-core/src/main/java/com/xxl/job/core/handler/IJobHandler.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/handler/IJobHandler.java
@@ -12,8 +12,7 @@ public abstract class IJobHandler extends HandlerRouter {
 	 * job handler <br><br>
 	 * the return Object will be and stored
 	 * @param params
-	 * @return void, fail if catch exception
-	 * @throws Exception
+	 * @throws Exception  default sussecc, fail if catch exception
 	 */
 	public abstract void execute(String... params) throws Exception;
 	
