73dc089e72743641e9cb6c608de4d2951a1197ef
kangxinghua <kangxinghua@aukeys.com>
2017-04-28 11:31:36 +0800

    新增spring-boot demo

---
+++ xxl-job-executor-example-boot/src/main/java/com/xxl/job/executor/config/XxlJobConfig.java
@@ -0,0 +1,12 @@
+package com.xxl.job.executor.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+
+@Configuration
+@ImportResource("classpath:applicationcontext-xxl-job.xml")
+public class XxlJobConfig {
+
+}
