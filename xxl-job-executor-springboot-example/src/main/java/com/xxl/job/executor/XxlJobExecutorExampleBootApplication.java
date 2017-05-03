74c5854898c2cf9a340b9536f6944c0b6c906ae5
kangxinghua <kangxinghua@aukeys.com>
2017-05-03 11:49:51 +0800

    1.6.2.1

---
+++ xxl-job-executor-springboot-example/src/main/java/com/xxl/job/executor/XxlJobExecutorExampleBootApplication.java
@@ -0,0 +1,12 @@
+package com.xxl.job.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExecutorExampleBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxlJobExecutorExampleBootApplication.class, args);
+	}
+}
