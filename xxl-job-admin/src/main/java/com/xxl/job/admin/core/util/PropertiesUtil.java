32c04851817508a447a4f2dcf86ee387177565fa
kevin <kevinz@weghst.com>
2017-10-20 16:24:45 +0800

    Auto stash before rebase of "origin/master"

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/util/PropertiesUtil.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/util/PropertiesUtil.java
@@ -1,41 +1,26 @@
 package com.xxl.job.admin.core.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.EncodedResource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
-
-import java.io.IOException;
-import java.util.Properties;
+import org.springframework.core.env.Environment;
 
 /**
  * properties util
+ *
  * @author xuxueli 2015-8-28 10:35:53
  */
 public class PropertiesUtil {
-	private static Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
-	private static final String file_name = "xxl-job-admin.properties";
 
+    private static PropertiesUtil instance = new PropertiesUtil();
+    private Environment environment;
 
-	public static String getString(String key) {
-		Properties prop = null;
-		try {
-			Resource resource = new ClassPathResource(file_name);
-			EncodedResource encodedResource = new EncodedResource(resource,"UTF-8");
-			prop = PropertiesLoaderUtils.loadProperties(encodedResource);
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-		}
-		if (prop!=null) {
-			return prop.getProperty(key);
-		}
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(getString("xxl.job.login.username"));
-	}
+    public static String getString(String key) {
+        return instance.environment.getProperty(key);
+    }
 
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    public static PropertiesUtil getInstance() {
+        return instance;
+    }
 }
