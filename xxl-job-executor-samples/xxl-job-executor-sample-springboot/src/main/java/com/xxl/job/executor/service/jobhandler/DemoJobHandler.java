fa1ed59b8d673844e4d8707a5ae32ac72edf28ce
xuxueli <931591021@qq.com>
2017-12-25 16:41:02 +0800

    clean code; 升级依赖版本；

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/DemoJobHandler.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/DemoJobHandler.java
@@ -4,7 +4,7 @@ import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
 import com.xxl.job.core.log.XxlJobLogger;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,15 +13,15 @@ import java.util.concurrent.TimeUnit;
  * 任务Handler的一个Demo（Bean模式）
  *
  * 开发步骤：
- * 1、继承 “IJobHandler” ；
- * 2、装配到Spring，例如加 “@Service” 注解；
- * 3、加 “@JobHandler” 注解，注解value值为新增任务生成的JobKey的值;多个JobKey用逗号分割;
+ * 1、新建一个继承com.xxl.job.core.handler.IJobHandler的Java类；
+ * 2、该类被Spring容器扫描为Bean实例，如加“@Component”注解；
+ * 3、添加 “@JobHandler(value="自定义jobhandler名称")”注解，注解的value值为自定义的JobHandler名称，该名称对应的是调度中心新建任务的JobHandler属性的值。
  * 4、执行日志：需要通过 "XxlJobLogger.log" 打印执行日志；
  *
  * @author xuxueli 2015-12-19 19:43:36
  */
 @JobHandler(value="demoJobHandler")
-@Service
+@Component
 public class DemoJobHandler extends IJobHandler {
 
 	@Override
