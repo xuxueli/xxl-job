0346ddbb91fe6d196bf22a088f4e40cf2daa70ea
xuxueli <931591021@qq.com>
2018-01-20 14:22:18 +0800

    后端代码国际化定制

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/util/I18nUtil.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/util/I18nUtil.java
@@ -1,6 +1,7 @@
 package com.xxl.job.admin.core.util;
 
 import com.xxl.job.core.util.JacksonUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
@@ -9,6 +10,7 @@ import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -21,15 +23,19 @@ import java.util.Properties;
 public class I18nUtil {
     private static Logger logger = LoggerFactory.getLogger(I18nUtil.class);
 
-    private static final String i18n_file = "i18n/message.properties";
-
     private static Properties prop = null;
     public static Properties loadI18nProp(){
         if (prop != null) {
             //return prop;
         }
         try {
-            Resource resource = new ClassPathResource(i18n_file);
+            // bild i18n prop
+            String i18n = PropertiesUtil.getString("xxl.job.i18n");
+            i18n = StringUtils.isNotBlank(i18n)?("_"+i18n):i18n;
+            String i18nFile =MessageFormat.format("i18n/message{0}.properties", i18n);
+
+            // load prop
+            Resource resource = new ClassPathResource(i18nFile);
             EncodedResource encodedResource = new EncodedResource(resource,"UTF-8");
             prop = PropertiesLoaderUtils.loadProperties(encodedResource);
         } catch (IOException e) {
