7c8d4a5395be72fea703868e15d8febf37fb2bad
sunrui849 <rui.849217@outlook.com>
2018-04-13 14:34:15 +0800

    Update XxlJobDynamicScheduler.java

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/schedule/XxlJobDynamicScheduler.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/schedule/XxlJobDynamicScheduler.java
@@ -23,6 +23,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.util.Assert;
+import com.xxl.job.admin.core.trigger.XxlJobTrigger;
+import org.apache.commons.lang3.StringUtils;
+
 
 import java.util.Date;
 import java.util.HashSet;
