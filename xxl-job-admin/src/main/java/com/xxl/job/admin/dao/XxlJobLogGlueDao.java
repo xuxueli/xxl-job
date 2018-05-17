26d2159a1177139dcdb26892cfdc0ccd50658011
Johnny <Johnny061088@gmail.com>
2018-05-17 15:48:14 +0800

    Remove "public" in interface and add "@Mapper" annotion

--- xxl-job-admin/src/main/java/com/xxl/job/admin/dao/XxlJobLogGlueDao.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/dao/XxlJobLogGlueDao.java
@@ -1,22 +1,25 @@
 package com.xxl.job.admin.dao;
 
 import com.xxl.job.admin.core.model.XxlJobLogGlue;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 /**
  * job log for glue
+ *
  * @author xuxueli 2016-5-19 18:04:56
  */
+@Mapper
 public interface XxlJobLogGlueDao {
-	
-	public int save(XxlJobLogGlue xxlJobLogGlue);
-	
-	public List<XxlJobLogGlue> findByJobId(@Param("jobId") int jobId);
 
-	public int removeOld(@Param("jobId") int jobId, @Param("limit") int limit);
+    int save(XxlJobLogGlue xxlJobLogGlue);
 
-	public int deleteByJobId(@Param("jobId") int jobId);
-	
+    List<XxlJobLogGlue> findByJobId(@Param("jobId") int jobId);
+
+    int removeOld(@Param("jobId") int jobId, @Param("limit") int limit);
+
+    int deleteByJobId(@Param("jobId") int jobId);
+
 }
--- xxl-job-admin/src/main/java/com/xxl/job/admin/dao/XxlJobRegistryDao.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/dao/XxlJobRegistryDao.java
@@ -1,6 +1,7 @@
 package com.xxl.job.admin.dao;
 
 import com.xxl.job.admin.core.model.XxlJobRegistry;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -8,22 +9,23 @@ import java.util.List;
 /**
  * Created by xuxueli on 16/9/30.
  */
+@Mapper
 public interface XxlJobRegistryDao {
 
-    public int removeDead(@Param("timeout") int timeout);
+    int removeDead(@Param("timeout") int timeout);
 
-    public List<XxlJobRegistry> findAll(@Param("timeout") int timeout);
+    List<XxlJobRegistry> findAll(@Param("timeout") int timeout);
 
-    public int registryUpdate(@Param("registryGroup") String registryGroup,
-                              @Param("registryKey") String registryKey,
-                              @Param("registryValue") String registryValue);
+    int registryUpdate(@Param("registryGroup") String registryGroup,
+                       @Param("registryKey") String registryKey,
+                       @Param("registryValue") String registryValue);
 
-    public int registrySave(@Param("registryGroup") String registryGroup,
-                            @Param("registryKey") String registryKey,
-                            @Param("registryValue") String registryValue);
+    int registrySave(@Param("registryGroup") String registryGroup,
+                     @Param("registryKey") String registryKey,
+                     @Param("registryValue") String registryValue);
 
-    public int registryDelete(@Param("registryGroup") String registGroup,
-                          @Param("registryKey") String registryKey,
-                          @Param("registryValue") String registryValue);
+    int registryDelete(@Param("registryGroup") String registGroup,
+                       @Param("registryKey") String registryKey,
+                       @Param("registryValue") String registryValue);
 
 }
