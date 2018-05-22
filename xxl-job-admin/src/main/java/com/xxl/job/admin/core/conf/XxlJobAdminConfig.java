1ff1e82de0cc1c61857329810bdaa75d18d3241f
Johnny <Johnny061088@gmail.com>
2018-05-22 14:27:20 +0800

    Add Spring boot starter

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/conf/XxlJobAdminConfig.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/conf/XxlJobAdminConfig.java
@@ -2,6 +2,7 @@ package com.xxl.job.admin.core.conf;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @author xuxueli 2017-04-28
  */
 @Configuration
+@ConfigurationProperties(prefix = "xxl.job")
 public class XxlJobAdminConfig implements InitializingBean{
     private static XxlJobAdminConfig adminConfig = null;
     public static XxlJobAdminConfig getAdminConfig() {
@@ -21,32 +23,23 @@ public class XxlJobAdminConfig implements InitializingBean{
         adminConfig = this;
     }
 
-    @Value("${xxl.job.mail.host}")
-    private String mailHost;
+    private String mailHost = "smtp.163.com";
 
-    @Value("${xxl.job.mail.port}")
-    private String mailPort;
+    private String mailPort = "25";
 
-    @Value("${xxl.job.mail.ssl}")
-    private boolean mailSSL;
+    private boolean mailSSL = false;
 
-    @Value("${xxl.job.mail.username}")
-    private String mailUsername;
+    private String mailUsername = "";
 
-    @Value("${xxl.job.mail.password}")
-    private String mailPassword;
+    private String mailPassword = "";
 
-    @Value("${xxl.job.mail.sendNick}")
-    private String mailSendNick;
+    private String mailSendNick = "";
 
-    @Value("${xxl.job.login.username}")
-    private String loginUsername;
+    private String loginUsername = "";
 
-    @Value("${xxl.job.login.password}")
-    private String loginPassword;
+    private String loginPassword = "";
 
-    @Value("${xxl.job.i18n}")
-    private String i18n;
+    private String i18n ="";
 
 
     public String getMailHost() {
