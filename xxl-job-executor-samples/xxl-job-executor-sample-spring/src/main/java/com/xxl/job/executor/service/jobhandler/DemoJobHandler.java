0dbd21abacea359f1458384434dd1c3a9f3d51d0
zhangkenan <zhangkenan@houbank.cn>
2018-06-22 18:09:50 +0800

    研究代码

--- xxl-job-executor-samples/xxl-job-executor-sample-spring/src/main/java/com/xxl/job/executor/service/jobhandler/DemoJobHandler.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-spring/src/main/java/com/xxl/job/executor/service/jobhandler/DemoJobHandler.java
@@ -28,7 +28,7 @@ public class DemoJobHandler extends IJobHandler {
 	public ReturnT<String> execute(String param) throws Exception {
 		XxlJobLogger.log("XXL-JOB, Hello World.");
 		
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 15; i++) {
 			XxlJobLogger.log("beat at:" + i);
 			TimeUnit.SECONDS.sleep(2);
 		}
