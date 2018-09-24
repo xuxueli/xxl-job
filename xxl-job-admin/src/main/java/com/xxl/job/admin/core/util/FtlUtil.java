78e135200c34966af62ba6fd63ca7de3db959e41
xuxueli <931591021@qq.com>
2018-09-24 23:26:13 +0800

    项目依赖全量升级至较新稳定版本，如spring、Jackson、groovy等等；

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/util/FtlUtil.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/util/FtlUtil.java
@@ -1,6 +1,8 @@
 package com.xxl.job.admin.core.util;
 
 import freemarker.ext.beans.BeansWrapper;
+import freemarker.ext.beans.BeansWrapperBuilder;
+import freemarker.template.Configuration;
 import freemarker.template.TemplateHashModel;
 
 /**
@@ -10,9 +12,10 @@ import freemarker.template.TemplateHashModel;
  */
 public class FtlUtil {
 
+    private static BeansWrapper wrapper = new BeansWrapperBuilder(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS).build();     //BeansWrapper.getDefaultInstance();
+
     public static TemplateHashModel generateStaticModel(String packageName) {
         try {
-            BeansWrapper wrapper = BeansWrapper.getDefaultInstance();
             TemplateHashModel staticModels = wrapper.getStaticModels();
             TemplateHashModel fileStatics = (TemplateHashModel) staticModels.get(packageName);
             return fileStatics;
