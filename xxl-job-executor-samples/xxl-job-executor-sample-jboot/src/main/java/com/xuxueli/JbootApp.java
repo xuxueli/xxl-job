c722ac1ae8908c07a8245f0f13fec22406a77f79
liuhongfeng <daliu_it@163.com>
2019-05-12 08:47:07 +0800

    添加Jboot框架的应用示例

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-jboot/src/main/java/com/xuxueli/JbootApp.java
@@ -0,0 +1,14 @@
+package com.xuxueli;
+
+import io.jboot.app.JbootApplication;
+
+/**
+ * Jboot app
+ *
+ * @author https://github.com/souvc
+ */
+public class JbootApp {
+    public static void main(String[] args) {
+        JbootApplication.run(args);
+    }
+}
