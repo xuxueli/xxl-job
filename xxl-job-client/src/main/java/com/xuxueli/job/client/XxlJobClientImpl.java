8546b6db5701bd565b88e592c0f1782d540b59b3
GerogeLeon <leongeroge@gmail.com>
2019-05-30 20:23:00 +0800

    switch to use apache http client to implement the xxljob client

--- xxl-job-client/src/main/java/com/xuxueli/job/client/XxlJobClientImpl.java
+++ xxl-job-client/src/main/java/com/xuxueli/job/client/XxlJobClientImpl.java
@@ -6,19 +6,23 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.xuxueli.job.client.model.XxlJobInfo;
 import com.xxl.job.core.biz.model.ReturnT;
-import org.apache.http.HttpEntity;
-import org.apache.http.StatusLine;
+import org.apache.http.*;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeaderElementIterator;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.Args;
 import org.springframework.beans.factory.DisposableBean;
 
 import java.io.ByteArrayOutputStream;
@@ -33,7 +37,13 @@ import java.util.Arrays;
  */
 public class XxlJobClientImpl implements XxlJobClient, DisposableBean {
     public static final String H_ACCESS_TOKEN = "ACCESS_TOKEN";
+    public static final String JOBOPS_URL_PART = "/jobops";
     public static final int HTTP_SUCCESS_CODE = 200;
+    public static final int CONN_MAX_LIVE_SECONDS = 5;
+    public static final int INACTIVITY_SECONDS_BEFORE_VALIDATE = 30;
+    public static final int MAX_CONNECTIONS = 10000;
+    public static final int MAX_CONNECTIONS_PER_ROUTE = 100;
+    public static final int TIMEOUT_IN_SEC = 3;
 
     private final CloseableHttpClient httpClient;
 
@@ -42,23 +52,59 @@ public class XxlJobClientImpl implements XxlJobClient, DisposableBean {
     private final ObjectWriter writer;
     private final ObjectReader reader;
     private final String accessToken;
+    private final PoolingHttpClientConnectionManager connectionManager;
 
 
     public XxlJobClientImpl(XxlJobProperties xxlJobProperties) {
-        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(100);
+        connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(MAX_CONNECTIONS);
+        connectionManager.setDefaultMaxPerRoute(MAX_CONNECTIONS_PER_ROUTE);
+        connectionManager.setValidateAfterInactivity(INACTIVITY_SECONDS_BEFORE_VALIDATE * 1000);
 
-        httpClient = HttpClients.custom().setConnectionManager(connectionManager).build();
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(TIMEOUT_IN_SEC * 1000)
+                .setConnectionRequestTimeout(TIMEOUT_IN_SEC * 1000)
+                .setSocketTimeout(TIMEOUT_IN_SEC * 1000).build();
+
+        httpClient = HttpClients.custom().setDefaultRequestConfig(requestConfig)
+                .setKeepAliveStrategy(new FiniteConnectionKeepAliveStrategy(CONN_MAX_LIVE_SECONDS))
+                .setConnectionManager(connectionManager).build();
 
         String serverAddresses = xxlJobProperties.getServerAddresses();
         accessToken = xxlJobProperties.getAccessToken();
-        baseUrl = serverAddresses + "/jobops";
+        baseUrl = serverAddresses + JOBOPS_URL_PART;
         ObjectMapper objectMapper = new ObjectMapper();
         writer = objectMapper.writer();
         TypeReference<ReturnT<String>> typeReference = new TypeReference<ReturnT<String>>() {
         };
         reader = objectMapper.readerFor(typeReference);
+    }
+
+    public static class FiniteConnectionKeepAliveStrategy implements ConnectionKeepAliveStrategy {
+
+        public final long connMaxLiveMilSec;
+
+        public FiniteConnectionKeepAliveStrategy(int connMaxLiveSeconds) {
+            this.connMaxLiveMilSec = connMaxLiveSeconds * 1000;
+        }
+
+        @Override
+        public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+            Args.notNull(response, "HTTP response");
+            final HeaderElementIterator it = new BasicHeaderElementIterator(
+                    response.headerIterator(HTTP.CONN_KEEP_ALIVE));
+            while (it.hasNext()) {
+                final HeaderElement he = it.nextElement();
+                final String param = he.getName();
+                final String value = he.getValue();
+                if (value != null && param.equalsIgnoreCase("timeout")) {
+                    try {
+                        return Long.parseLong(value) * 1000;
+                    } catch (final NumberFormatException ignore) {
+                    }
+                }
+            }
+            return connMaxLiveMilSec;
+        }
     }
 
     @Override
@@ -113,7 +159,11 @@ public class XxlJobClientImpl implements XxlJobClient, DisposableBean {
 
     @Override
     public void destroy() throws Exception {
-        httpClient.close();
+        try {
+            httpClient.close();
+        } finally {
+            connectionManager.close();
+        }
     }
 
     private ReturnT<String> opsTemplate(String ops, HttpEntity entity) throws IOException {
