ec0ff8da867a59035c043bd8f29eb31e98b6d764
paopaofish <994987301@qq.com>
2019-10-22 23:10:10 +0800

    Using non-final variables to lock can cause thread-safety problems.According to my understanding, it is appropriate to add the final keyword here

--- xxl-job-core/src/main/java/com/xxl/job/core/util/DateUtil.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/util/DateUtil.java
@@ -24,7 +24,7 @@ public class DateUtil {
     private static final String DATE_FORMAT = "yyyy-MM-dd";
     private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-    private static ThreadLocal<Map<String, DateFormat>> dateFormatThreadLocal = new ThreadLocal<Map<String, DateFormat>>();
+    private static final ThreadLocal<Map<String, DateFormat>> dateFormatThreadLocal = new ThreadLocal<Map<String, DateFormat>>();
     private static DateFormat getDateFormat(String pattern) {
         if (pattern==null || pattern.trim().length()==0) {
             throw new IllegalArgumentException("pattern cannot be empty.");
