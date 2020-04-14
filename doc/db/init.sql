1ce96141291f064c83766421c64278fbf543eb32
yunsongliu <YunSongLiu@kaifa.cn>
2020-04-14 09:04:49 +0800

    新增primary_key_gen的初始化语句

--- doc/db/init.sql
+++ doc/db/init.sql
@@ -4,4 +4,4 @@ CREATE TABLE xxl_job_lock (
 );
 
 INSERT INTO xxl_job_lock ( lock_name) VALUES ( 'schedule_lock');
-INSERT INTO kaifa_job_user(id, username, password, role, permission) VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 1, NULL);
+INSERT INTO xxl_job_user(id, username, password, role, permission) VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 1, NULL);
