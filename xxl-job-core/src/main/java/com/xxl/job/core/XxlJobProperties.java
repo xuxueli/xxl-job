eb9249c42bcb817fbc3c582062e43b142c6063b2
xiaoyifang <yifang.xiao@gmail.com>
2020-04-28 22:59:41 +0800

    使用spring boot 默认端口，不用netty

--- xxl-job-core/src/main/java/com/xxl/job/core/XxlJobProperties.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/XxlJobProperties.java
@@ -7,50 +7,23 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties(prefix = "xxl.job")
 public class XxlJobProperties {
-
     private AdminProperties admin = new AdminProperties();
-
     private ExecutorProperties executor = new ExecutorProperties();
-
 
     @Data
     static public class AdminProperties {
-
         private String addresses;
     }
 
-
     @Data
     static public class ExecutorProperties {
-
         private String appname;
-
         private String ip;
-
         private Integer port = 9999;
-
         private String accessToken;
-
         private String logPath;
-
         private Integer logRetentionDays = 30;
         private boolean integratedSpringBoot;
         private String address;
-
-        public boolean getIntegratedSpringBoot() {
-            return integratedSpringBoot;
-        }
-
-        public void setIntegratedSpringBoot(boolean integratedSpringBoot) {
-            this.integratedSpringBoot = integratedSpringBoot;
-        }
-
-        public String getAddress() {
-            return address;
-        }
-
-        public void setAddress(String address) {
-            this.address = address;
-        }
     }
 }
