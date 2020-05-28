c6ed6ac8af178a6a604cf7e3f04fbe516816a4ad
jacobs <jacobs_lei@163.com>
2020-05-28 18:18:46 +0800

    fixed issues： xxl_job_log_report表重复插入问题 #1650

--- doc/db/tables_xxl_job.sql
+++ doc/db/tables_xxl_job.sql
@@ -60,6 +60,8 @@ CREATE TABLE `xxl_job_log_report` (
   `running_count` int(11) NOT NULL DEFAULT '0' COMMENT '运行中-日志数量',
   `suc_count` int(11) NOT NULL DEFAULT '0' COMMENT '执行成功-日志数量',
   `fail_count` int(11) NOT NULL DEFAULT '0' COMMENT '执行失败-日志数量',
+  `add_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `i_trigger_day` (`trigger_day`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
