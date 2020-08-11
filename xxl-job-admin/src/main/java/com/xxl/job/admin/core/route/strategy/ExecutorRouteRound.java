b2ff5bedc8e12f3af9950ecea8dc27487597bff4
xuxueli <931591021@qq.com>
2020-08-11 16:41:08 +0800

    轮训路由策略优化，修复小概率下并发问题；

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/route/strategy/ExecutorRouteRound.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/route/strategy/ExecutorRouteRound.java
@@ -26,8 +26,8 @@ public class ExecutorRouteRound extends ExecutorRouter {
         }
 
         AtomicInteger count = routeCountEachJob.get(jobId);
-        // 初始化时主动Random一次，缓解首次压力
         if (count == null || count.get() > 1000000) {
+            // 初始化时主动Random一次，缓解首次压力
             count = new AtomicInteger(new Random().nextInt(100));
         } else {
             // count++
