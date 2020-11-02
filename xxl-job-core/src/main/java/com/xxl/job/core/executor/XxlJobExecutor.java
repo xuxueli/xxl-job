4a8e0f10f911a7258fbdadf84f4f378394e704fd
xueli.xue <xueli.xue@dianping.com>
2020-11-02 20:05:42 +0800

    执行器鉴权校验：执行器启动时主动校验accessToken，为空则主动Warn告警

--- xxl-job-core/src/main/java/com/xxl/job/core/executor/XxlJobExecutor.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/executor/XxlJobExecutor.java
@@ -147,6 +147,11 @@ public class XxlJobExecutor  {
             address = "http://{ip_port}/".replace("{ip_port}", ip_port_address);
         }
 
+        // accessToken
+        if (accessToken==null || accessToken.trim().length()==0) {
+            logger.warn(">>>>>>>>>>> xxl-job accessToken is empty. To ensure system security, please set the accessToken.");
+        }
+
         // start
         embedServer = new EmbedServer();
         embedServer.start(address, port, appname, accessToken);
