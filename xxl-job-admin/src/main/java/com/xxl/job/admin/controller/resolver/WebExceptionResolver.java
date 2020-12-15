8a5dd6f901d6318f2da6c11d64e7fafcc31f9368
如梦技术 <596392912@qq.com>
2020-12-15 16:24:49 +0800

    :sparkles: 优化。

--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/resolver/WebExceptionResolver.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/resolver/WebExceptionResolver.java
@@ -2,8 +2,11 @@ package com.xxl.job.admin.controller.resolver;
 
 import com.xxl.job.admin.core.exception.XxlJobException;
 import com.xxl.job.core.biz.model.ReturnT;
+import net.dreamlu.mica.core.utils.Charsets;
+import net.dreamlu.mica.core.utils.JsonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.HandlerMethod;
@@ -21,44 +24,42 @@ import java.io.IOException;
  */
 @Component
 public class WebExceptionResolver implements HandlerExceptionResolver {
-	private static transient Logger logger = LoggerFactory.getLogger(WebExceptionResolver.class);
+    private static transient Logger logger = LoggerFactory.getLogger(WebExceptionResolver.class);
 
-	@Override
-	public ModelAndView resolveException(HttpServletRequest request,
-			HttpServletResponse response, Object handler, Exception ex) {
-		if (!(ex instanceof XxlJobException)) {
-			logger.error("WebExceptionResolver:", ex);
-		}
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request,
+                                         HttpServletResponse response, Object handler, Exception ex) {
+        if (!(ex instanceof XxlJobException)) {
+            logger.error("WebExceptionResolver:", ex);
+        }
 
-		// if json
-		boolean isJson = false;
-		if (handler instanceof HandlerMethod) {
-			HandlerMethod method = (HandlerMethod)handler;
-			ResponseBody responseBody = method.getMethodAnnotation(ResponseBody.class);
-			if (responseBody != null) {
-				isJson = true;
-			}
-		}
-
-		// error result
-		ReturnT<String> errorResult = new ReturnT<String>(ReturnT.FAIL_CODE, ex.toString().replaceAll("\n", "<br/>"));
-
-		// response
-		ModelAndView mv = new ModelAndView();
-		if (isJson) {
-			try {
-				response.setContentType("application/json;charset=utf-8");
-				response.getWriter().print(JacksonUtil.writeValueAsString(errorResult));
-			} catch (IOException e) {
-				logger.error(e.getMessage(), e);
-			}
-			return mv;
-		} else {
-
-			mv.addObject("exceptionMsg", errorResult.getMsg());
-			mv.setViewName("/common/common.exception");
-			return mv;
-		}
-	}
+        // if json
+        boolean isJson = false;
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod method = (HandlerMethod) handler;
+            ResponseBody responseBody = method.getMethodAnnotation(ResponseBody.class);
+            if (responseBody != null) {
+                isJson = true;
+            }
+        }
+        // error result
+        ReturnT<String> errorResult = new ReturnT<>(ReturnT.FAIL_CODE, ex.toString().replaceAll("\n", "<br/>"));
+        // response
+        ModelAndView mv = new ModelAndView();
+        if (isJson) {
+            try {
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                response.setCharacterEncoding(Charsets.UTF_8_NAME);
+                response.getWriter().print(JsonUtil.toJson(errorResult));
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+            }
+            return mv;
+        } else {
+            mv.addObject("exceptionMsg", errorResult.getMsg());
+            mv.setViewName("/common/common.exception");
+            return mv;
+        }
+    }
 
 }
