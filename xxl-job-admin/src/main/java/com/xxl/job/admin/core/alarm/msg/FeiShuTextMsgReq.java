d0c0c3f804642f7529ed0963ecf63a7221784e54
fuyousheng <fuyousheng>
2022-02-23 17:17:18 +0800

    修改飞书样式

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/alarm/msg/FeiShuTextMsgReq.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/alarm/msg/FeiShuTextMsgReq.java
@@ -15,13 +15,21 @@ import java.util.Map;
  */
 public class FeiShuTextMsgReq extends BaseMsg {
 
+
     public FeiShuTextMsgReq() {
         List<Map<String, Object>> elements = new ArrayList<>();
         Map<String, Boolean> config = new HashMap<>();
         config.put("wide_screen_mode", true);
         card.put("config", config);
         card.put("elements", elements);
+        card.put("header", header);
         text.put("tag", "markdown");
+        //header
+        header.put("template", "green");
+        header.put("title", headTxt);
+
+        headTxt.put("tag", "plain_text");
+        headTxt.put("content", "");
         elements.add(text);
     }
 
@@ -32,7 +40,16 @@ public class FeiShuTextMsgReq extends BaseMsg {
     private Map<String, Object> text = new HashMap<>();
 
     @JsonProperty(value = "msg_type")
-    private String msgType = "interactive";
+    private String msg_type = "interactive";
+
+
+    @JsonIgnore
+    private Map<String, Object> header = new HashMap<>();
+
+    @JsonIgnore
+    private Map<String, Object> headTxt = new HashMap<>();
+
+
 
     /**
      * 设置用户
@@ -45,20 +62,21 @@ public class FeiShuTextMsgReq extends BaseMsg {
         return this;
     }
 
-    public String getMsgType() {
-        return msgType;
+    @JsonIgnore
+    public FeiShuTextMsgReq withTitle(String title) {
+        headTxt.put("content",title);
+        return this;
     }
 
-    public void setMsgType(String msgType) {
-        this.msgType = msgType;
+
+    @JsonIgnore
+    public FeiShuTextMsgReq withTitleColor(String color) {
+        header.put("template", color);
+        return this;
     }
 
     public Map<String, Object> getCard() {
         return card;
-    }
-
-    public void setCard(Map<String, Object> card) {
-        this.card = card;
     }
 
     @Override
