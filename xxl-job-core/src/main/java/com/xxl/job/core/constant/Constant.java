7a8dca297f7f6774ae50e64b306c5fcc82b4e3c2
ckasdf1405 <zhengcczd@163.com>
2022-06-09 18:49:00 +0800

    删除常量定义，已不使用

--- xxl-job-core/src/main/java/com/xxl/job/core/constant/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.xxl.job.core.constant;
-
-/**
- * @author zheng
- */
-public interface Constant {
-    public static final String INSERT = "INSERT";
-    public static final String UPDATE = "UPDATE";
-}
