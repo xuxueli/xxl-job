752384d618b323d93173e447dbdf91e1973e0819
buzhidaolvtu <buzhidaolvtu@163.com>
2023-04-20 22:52:01 +0800

    以APP为范围使用LRU和LFU

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/route/strategy/ExecutorRouteLFU.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/route/strategy/ExecutorRouteLFU.java
@@ -1,5 +1,6 @@
 package com.xxl.job.admin.core.route.strategy;
 
+import com.xxl.job.admin.core.model.XxlJobGroup;
 import com.xxl.job.admin.core.route.ExecutorRouter;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.biz.model.TriggerParam;
@@ -10,70 +11,108 @@ import java.util.concurrent.ConcurrentMap;
 
 /**
  * 单个JOB对应的每个执行器，使用频率最低的优先被选举
- *      a(*)、LFU(Least Frequently Used)：最不经常使用，频率/次数
- *      b、LRU(Least Recently Used)：最近最久未使用，时间
- *
+ * a(*)、LFU(Least Frequently Used)：最不经常使用，频率/次数
+ * b、LRU(Least Recently Used)：最近最久未使用，时间
+ * <p>
  * Created by xuxueli on 17/3/10.
  */
 public class ExecutorRouteLFU extends ExecutorRouter {
 
-    private static ConcurrentMap<Integer, HashMap<String, Integer>> jobLfuMap = new ConcurrentHashMap<Integer, HashMap<String, Integer>>();
-    private static long CACHE_VALID_TIME = 0;
+    public static class AppAddressPool {
+        private static ConcurrentMap<String, AppAddressPool> APP_LFU_MANAGER = new ConcurrentHashMap();
 
-    public String route(int jobId, List<String> addressList) {
+        /**
+         * 每个APP name 实例化一个地址池
+         *
+         * @param appname
+         * @return
+         */
+        public static AppAddressPool getOrCreate(String appname) {
+            return APP_LFU_MANAGER.computeIfAbsent(appname, key -> new AppAddressPool());
+        }
 
+        private List<String> freeAddressList = new ArrayList<>();
+
+        //key=address
+        private HashMap<String, Integer> usedAddressManager = new HashMap<>();
+
+        private synchronized void clear(String appname) {
+            APP_LFU_MANAGER.remove(appname);
+            getOrCreate(appname);
+        }
+
+        public synchronized void updateAddressStats(String address) {
+            usedAddressManager.put(address, usedAddressManager.getOrDefault(address, 0) + 1);
+        }
+
+        private synchronized String route(List<String> addressList) {
+            HashMap<String, Integer> lfuItemMap = usedAddressManager;
+
+            // remove dead address
+            List<String> delKeys = new ArrayList<>();
+            for (String existKey : lfuItemMap.keySet()) {
+                if (!addressList.contains(existKey)) {
+                    delKeys.add(existKey);
+                }
+            }
+            if (delKeys.size() > 0) {
+                for (String delKey : delKeys) {
+                    lfuItemMap.remove(delKey);
+                }
+            }
+
+            // put new address into free list
+            freeAddressList.clear();
+            for (String address : addressList) {
+                if (!lfuItemMap.containsKey(address)) {
+                    freeAddressList.add(address);
+                }
+            }
+
+            if (freeAddressList.size() > 0) {
+                String freeAddress = freeAddressList.get(0);
+                freeAddressList.remove(0);
+                return freeAddress;
+            }
+
+            // load least userd count address
+            List<Map.Entry<String, Integer>> lfuItemList = new ArrayList<Map.Entry<String, Integer>>(lfuItemMap.entrySet());
+            Collections.sort(lfuItemList, new Comparator<Map.Entry<String, Integer>>() {
+                @Override
+                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                    return o1.getValue().compareTo(o2.getValue());
+                }
+            });
+
+            Map.Entry<String, Integer> addressItem = lfuItemList.get(0);
+
+            return addressItem.getKey();
+        }
+
+    }
+
+    private String route(String appname, List<String> addressList) {
+        AppAddressPool appAddressPool = AppAddressPool.getOrCreate(appname);
         // cache clear
         if (System.currentTimeMillis() > CACHE_VALID_TIME) {
-            jobLfuMap.clear();
-            CACHE_VALID_TIME = System.currentTimeMillis() + 1000*60*60*24;
+            appAddressPool.clear(appname);
+            CACHE_VALID_TIME = System.currentTimeMillis() + 1000 * 60 * 60 * 24;
         }
 
-        // lfu item init
-        HashMap<String, Integer> lfuItemMap = jobLfuMap.get(jobId);     // Key排序可以用TreeMap+构造入参Compare；Value排序暂时只能通过ArrayList；
-        if (lfuItemMap == null) {
-            lfuItemMap = new HashMap<String, Integer>();
-            jobLfuMap.putIfAbsent(jobId, lfuItemMap);   // 避免重复覆盖
-        }
+        return appAddressPool.route(addressList);
+    }
 
-        // put new
-        for (String address: addressList) {
-            if (!lfuItemMap.containsKey(address) || lfuItemMap.get(address) >1000000 ) {
-                lfuItemMap.put(address, new Random().nextInt(addressList.size()));  // 初始化时主动Random一次，缓解首次压力
-            }
-        }
-        // remove old
-        List<String> delKeys = new ArrayList<>();
-        for (String existKey: lfuItemMap.keySet()) {
-            if (!addressList.contains(existKey)) {
-                delKeys.add(existKey);
-            }
-        }
-        if (delKeys.size() > 0) {
-            for (String delKey: delKeys) {
-                lfuItemMap.remove(delKey);
-            }
-        }
+    private static long CACHE_VALID_TIME = 0;
 
-        // load least userd count address
-        List<Map.Entry<String, Integer>> lfuItemList = new ArrayList<Map.Entry<String, Integer>>(lfuItemMap.entrySet());
-        Collections.sort(lfuItemList, new Comparator<Map.Entry<String, Integer>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
-
-        Map.Entry<String, Integer> addressItem = lfuItemList.get(0);
-        String minAddress = addressItem.getKey();
-        addressItem.setValue(addressItem.getValue() + 1);
-
-        return addressItem.getKey();
+    @Override
+    public ReturnT<String> route(XxlJobGroup group, TriggerParam triggerParam, List<String> addressList) {
+        String address = route(group.getAppname(), addressList);
+        return new ReturnT<String>(address);
     }
 
     @Override
     public ReturnT<String> route(TriggerParam triggerParam, List<String> addressList) {
-        String address = route(triggerParam.getJobId(), addressList);
-        return new ReturnT<String>(address);
+        throw new RuntimeException("remove");
     }
 
 }
