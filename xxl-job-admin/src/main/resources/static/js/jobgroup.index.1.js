79aad210a977b4b2a4dcddde78337ecb521a4f70
黄展 <huangzhan@inspur.com>
2023-07-31 16:09:37 +0800

    修改appName和title的长度

--- xxl-job-admin/src/main/resources/static/js/jobgroup.index.1.js
+++ xxl-job-admin/src/main/resources/static/js/jobgroup.index.1.js
@@ -198,12 +198,12 @@ $(function() {
 		rules : {
 			appname : {
 				required : true,
-				rangelength:[4,64],
+				rangelength:[4,30],
 				myValid01 : true
 			},
 			title : {
 				required : true,
-				rangelength:[4, 12]
+				rangelength:[4, 50]
 			}
 		},
 		messages : {
@@ -295,12 +295,12 @@ $(function() {
 		rules : {
 			appname : {
 				required : true,
-				rangelength:[4,64],
+				rangelength:[4,30],
 				myValid01 : true
 			},
 			title : {
 				required : true,
-				rangelength:[4, 12]
+				rangelength:[4, 50]
 			}
 		},
 		messages : {
