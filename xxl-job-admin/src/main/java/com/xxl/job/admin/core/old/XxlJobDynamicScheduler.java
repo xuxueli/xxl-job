449b185bb7c25ebdb90cb8f8c7eb8be92500424a
zfm <zfm@enjoy.ai>
2023-08-10 17:13:15 +0800

    升级springboot版本到3.1.2

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/old/XxlJobDynamicScheduler.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/old/XxlJobDynamicScheduler.java
@@ -25,9 +25,9 @@
 //import org.slf4j.LoggerFactory;
 //import org.springframework.util.Assert;
 //
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
 //import java.io.IOException;
 //import java.util.Date;
 //import java.util.concurrent.ConcurrentHashMap;
