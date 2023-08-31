5929ea4280fd6c72e8184351d947ee5db5effd43
Rong.Jia <852203465@qq.com>
2023-08-31 10:01:21 +0800

    feature(server): 增加时间div

--- xxl-job-admin/src/main/resources/static/js/index.js
+++ xxl-job-admin/src/main/resources/static/js/index.js
@@ -3,6 +3,33 @@ $(function (){
     $("#current").text(account);
 })
 
+function getTime(){
+    var myDate = new Date();
+    var myYear = myDate.getFullYear(); //获取完整的年份(4位,1970-????)
+    var myMonth = myDate.getMonth()+1; //获取当前月份(0-11,0代表1月)
+    var myToday = myDate.getDate(); //获取当前日(1-31)
+    var myDay = myDate.getDay(); //获取当前星期X(0-6,0代表星期天)
+    var myHour = myDate.getHours(); //获取当前小时数(0-23)
+    var myMinute = myDate.getMinutes(); //获取当前分钟数(0-59)
+    var mySecond = myDate.getSeconds(); //获取当前秒数(0-59)
+    var week = ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'];
+    var nowTime;
+
+    nowTime = myYear+'-'+fillZero(myMonth)+'-'+fillZero(myToday)+'&nbsp;&nbsp;'+week[myDay]+'&nbsp;&nbsp;'+fillZero(myHour)+':'+fillZero(myMinute)+':'+fillZero(mySecond);
+    //console.log(nowTime);
+    $('#time').html(nowTime);
+}
+function fillZero(str){
+    var realNum;
+    if(str<10){
+        realNum	= '0'+str;
+    }else{
+        realNum	= str;
+    }
+    return realNum;
+}
+setInterval(getTime,1000);
+
 /**
  * 修改密码
  */
