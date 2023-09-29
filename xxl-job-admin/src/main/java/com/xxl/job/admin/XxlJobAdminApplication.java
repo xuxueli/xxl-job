78230734a4af1e8bc4a0af8768b486a3a73a5ea3
Rong.Jia <852203465@qq.com>
2023-09-29 13:00:34 +0800

    feature(server): 增加注释

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -4,6 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 服务启动类
+ *
+ * @author Rong.Jia
+ * @date 2023/09/29
+ */
 @MapperScan("com.xxl.job.admin.mapper")
 @SpringBootApplication
 public class XxlJobAdminApplication {
