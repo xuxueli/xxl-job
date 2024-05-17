675866990571b1d9dee91da95871a39a1312d224
tbc <tbc521@163.com>
2024-05-17 22:53:34 +0800

    XxlJobSpringExecutor 提供是否立即start选项供用户使用，在某些客户端有自己的初始化逻辑的时候，用户可自己控制未完成初始化时调度指令就过来的情况

--- xxl-job-core/src/main/java/com/xxl/job/core/executor/impl/XxlJobSpringExecutor.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/executor/impl/XxlJobSpringExecutor.java
@@ -26,6 +26,15 @@ import java.util.Map;
 public class XxlJobSpringExecutor extends XxlJobExecutor implements ApplicationContextAware, SmartInitializingSingleton, DisposableBean {
     private static final Logger logger = LoggerFactory.getLogger(XxlJobSpringExecutor.class);
 
+    private final boolean atOnceStart;
+
+    public XxlJobSpringExecutor() {
+        this(true);
+    }
+
+    public XxlJobSpringExecutor(boolean atOnceStart) {
+        this.atOnceStart = atOnceStart;
+    }
 
     // start
     @Override
@@ -41,10 +50,12 @@ public class XxlJobSpringExecutor extends XxlJobExecutor implements ApplicationC
         GlueFactory.refreshInstance(1);
 
         // super start
-        try {
-            super.start();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        if (atOnceStart) {
+            try {
+                super.start();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
