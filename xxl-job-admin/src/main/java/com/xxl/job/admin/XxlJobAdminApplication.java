fe5a07e64c603ab6bfc8c155a9842e0bf0b4f9d1
zc888168 <passion888168@126.com>
2024-06-21 17:54:48 +0800

    Update XxlJobAdminApplication.java
    提供了添加自定义监听器的能力，适合需要更多定制化配置的场景，比如需要记录PID或暴露端口信息

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -2,6 +2,8 @@ package com.xxl.job.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.context.WebServerPortFileWriter;
 
 /**
  * @author xuxueli 2018-10-28 00:38:13
@@ -10,7 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XxlJobAdminApplication {
 
 	public static void main(String[] args) {
-        SpringApplication.run(XxlJobAdminApplication.class, args);
-	}
+	    SpringApplication app = new SpringApplication(XxlJobAdminApplication.class);
+	    app.addListeners(new ApplicationPidFileWriter(), new WebServerPortFileWriter());
+	    app.run(args);
+      }
 
 }
