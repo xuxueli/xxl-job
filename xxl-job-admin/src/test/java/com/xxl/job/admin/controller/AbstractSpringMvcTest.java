48fb53ca501e4d42786466ed865ea021f90f36f1
CodePlayer <codeplayer360@gmail.com>
2024-11-06 20:39:03 +0800

    改进：避免使用不推荐的 @Autowired

--- xxl-job-admin/src/test/java/com/xxl/job/admin/controller/AbstractSpringMvcTest.java
+++ xxl-job-admin/src/test/java/com/xxl/job/admin/controller/AbstractSpringMvcTest.java
@@ -1,7 +1,8 @@
 package com.xxl.job.admin.controller;
 
+import javax.annotation.Resource;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -10,9 +11,9 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AbstractSpringMvcTest {
 
-  @Autowired
-  private WebApplicationContext applicationContext;
   protected MockMvc mockMvc;
+  @Resource
+  private WebApplicationContext applicationContext;
 
   @BeforeEach
   public void setup() {
