1341d872963c4ff6096c8b31db2dddf2dc8e9ce3
CodePlayer <codeplayer360@gmail.com>
2024-11-06 20:37:12 +0800

    改进：NetUtil 实现细节

--- xxl-job-core/src/main/java/com/xxl/job/core/util/NetUtil.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/util/NetUtil.java
@@ -1,10 +1,10 @@
 package com.xxl.job.core.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.net.ServerSocket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * net util
@@ -12,59 +12,40 @@ import java.net.ServerSocket;
  * @author xuxueli 2017-11-29 17:00:25
  */
 public class NetUtil {
-    private static Logger logger = LoggerFactory.getLogger(NetUtil.class);
 
-    /**
-     * find avaliable port
-     *
-     * @param defaultPort
-     * @return
-     */
-    public static int findAvailablePort(int defaultPort) {
-        int portTmp = defaultPort;
-        while (portTmp < 65535) {
-            if (!isPortUsed(portTmp)) {
-                return portTmp;
-            } else {
-                portTmp++;
-            }
-        }
-        portTmp = defaultPort--;
-        while (portTmp > 0) {
-            if (!isPortUsed(portTmp)) {
-                return portTmp;
-            } else {
-                portTmp--;
-            }
-        }
-        throw new RuntimeException("no available port.");
-    }
+	private static final Logger logger = LoggerFactory.getLogger(NetUtil.class);
 
-    /**
-     * check port used
-     *
-     * @param port
-     * @return
-     */
-    public static boolean isPortUsed(int port) {
-        boolean used = false;
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(port);
-            used = false;
-        } catch (IOException e) {
-            logger.info(">>>>>>>>>>> xxl-job, port[{}] is in use.", port);
-            used = true;
-        } finally {
-            if (serverSocket != null) {
-                try {
-                    serverSocket.close();
-                } catch (IOException e) {
-                    logger.info("");
-                }
-            }
-        }
-        return used;
-    }
+	/**
+	 * find avaliable port
+	 */
+	public static int findAvailablePort(int defaultPort) {
+		int port = defaultPort;
+		while (port < 65535) {
+			if (isPortAvailable(port)) {
+				return port;
+			}
+			port++;
+		}
+		port = defaultPort - 1;
+		while (port > 0) {
+			if (isPortAvailable(port)) {
+				return port;
+			}
+			port--;
+		}
+		throw new RuntimeException("no available port.");
+	}
+
+	/**
+	 * check port used
+	 */
+	public static boolean isPortAvailable(int port) {
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
+			return true;
+		} catch (IOException ignored) {
+			logger.info(">>>>>>>>>>> xxl-job, port[{}] is in use.", port);
+			return false;
+		}
+	}
 
 }
