51d9732fbbb4b18a1e78f2a774505d6ca130c142
CodePlayer <codeplayer360@gmail.com>
2024-11-08 18:13:17 +0800

    改进：优化SSL上下文处理

--- xxl-job-core/src/main/java/com/xxl/job/core/util/XxlJobRemotingUtil.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/util/XxlJobRemotingUtil.java
@@ -18,84 +18,90 @@ import org.springframework.util.StringUtils;
  */
 public class XxlJobRemotingUtil {
 
-    private static Logger logger = LoggerFactory.getLogger(XxlJobRemotingUtil.class);
-    public static final String XXL_JOB_ACCESS_TOKEN = "XXL-JOB-ACCESS-TOKEN";
+	private static final Logger logger = LoggerFactory.getLogger(XxlJobRemotingUtil.class);
+	public static final String XXL_JOB_ACCESS_TOKEN = "XXL-JOB-ACCESS-TOKEN";
+	private static SSLSocketFactory sslSocketFactory;
 
-    // trust-https start
-    private static void trustAllHosts(HttpsURLConnection connection) {
-        try {
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            SSLSocketFactory newFactory = sc.getSocketFactory();
+	// trust-https start
+	private static void trustAllHosts(HttpsURLConnection connection) {
+		// 内网一般是 http，无需直接静态初始化，需要时才懒加载并缓存工厂类
+		SSLSocketFactory socketFactory = sslSocketFactory;
+		if (socketFactory == null) {
+			try {
+				final SSLContext sslContext = SSLContext.getInstance("TLS");
+				sslContext.init(null, new TrustManager[] {
+						new X509TrustManager() {
+							@Override
+							public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+								return new java.security.cert.X509Certificate[] {};
+							}
 
-            connection.setSSLSocketFactory(newFactory);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-        connection.setHostnameVerifier((hostname, session) -> true);
-    }
+							@Override
+							public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+							}
 
-    private static final TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-        @Override
-        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-            return new java.security.cert.X509Certificate[] {};
-        }
+							@Override
+							public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+							}
+						}
+				}, new java.security.SecureRandom());
+				sslSocketFactory = socketFactory = sslContext.getSocketFactory();
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
+		if (socketFactory != null) {
+			connection.setSSLSocketFactory(socketFactory);
+		}
+		connection.setHostnameVerifier((hostname, session) -> true);
+	}
+	// trust-https end
 
-        @Override
-        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        }
+	/**
+	 * post
+	 */
+	public static ReturnT postBody(String url, String accessToken, int timeout, Object requestObj, Class returnTargClassOfT) {
+		HttpURLConnection connection = null;
+		BufferedReader bufferedReader = null;
+		try {
+			// connection
+			URL realUrl = new URL(url);
+			connection = (HttpURLConnection) realUrl.openConnection();
 
-        @Override
-        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        }
-    } };
-    // trust-https end
+			// trust-https
+			boolean useHttps = url.startsWith("https");
+			if (useHttps) {
+				HttpsURLConnection https = (HttpsURLConnection) connection;
+				trustAllHosts(https);
+			}
 
-    /**
-     * post
-     */
-    public static ReturnT postBody(String url, String accessToken, int timeout, Object requestObj, Class returnTargClassOfT) {
-        HttpURLConnection connection = null;
-        BufferedReader bufferedReader = null;
-        try {
-            // connection
-            URL realUrl = new URL(url);
-            connection = (HttpURLConnection) realUrl.openConnection();
+			// connection setting
+			connection.setRequestMethod("POST");
+			connection.setDoOutput(true);
+			connection.setDoInput(true);
+			connection.setUseCaches(false);
+			connection.setReadTimeout(timeout * 1000);
+			connection.setConnectTimeout(3 * 1000);
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+			connection.setRequestProperty("Accept-Charset", "application/json;charset=UTF-8");
 
-            // trust-https
-            boolean useHttps = url.startsWith("https");
-            if (useHttps) {
-                HttpsURLConnection https = (HttpsURLConnection) connection;
-                trustAllHosts(https);
-            }
+			if (StringUtils.hasText(accessToken)) {
+				connection.setRequestProperty(XXL_JOB_ACCESS_TOKEN, accessToken);
+			}
 
-            // connection setting
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setUseCaches(false);
-            connection.setReadTimeout(timeout * 1000);
-            connection.setConnectTimeout(3 * 1000);
-            connection.setRequestProperty("connection", "Keep-Alive");
-            connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-            connection.setRequestProperty("Accept-Charset", "application/json;charset=UTF-8");
+			// do connection
+			connection.connect();
 
-            if (StringUtils.hasText(accessToken)) {
-                connection.setRequestProperty(XXL_JOB_ACCESS_TOKEN, accessToken);
-            }
+			// write requestBody
+			if (requestObj != null) {
+				String requestBody = GsonTool.toJson(requestObj);
 
-            // do connection
-            connection.connect();
-
-            // write requestBody
-            if (requestObj != null) {
-                String requestBody = GsonTool.toJson(requestObj);
-
-                DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
-                dataOutputStream.write(requestBody.getBytes(StandardCharsets.UTF_8));
-                dataOutputStream.flush();
-                dataOutputStream.close();
-            }
+				DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+				dataOutputStream.write(requestBody.getBytes(StandardCharsets.UTF_8));
+				dataOutputStream.flush();
+				dataOutputStream.close();
+			}
 
             /*byte[] requestBodyBytes = requestBody.getBytes("UTF-8");
             connection.setRequestProperty("Content-Length", String.valueOf(requestBodyBytes.length));
@@ -104,45 +110,44 @@ public class XxlJobRemotingUtil {
             outwritestream.flush();
             outwritestream.close();*/
 
-            // valid StatusCode
-            int statusCode = connection.getResponseCode();
-            if (statusCode != 200) {
-                return new ReturnT<String>(ReturnT.FAIL_CODE, "xxl-job remoting fail, StatusCode(" + statusCode + ") invalid. for url : " + url);
-            }
+			// valid StatusCode
+			int statusCode = connection.getResponseCode();
+			if (statusCode != 200) {
+				return new ReturnT<String>(ReturnT.FAIL_CODE, "xxl-job remoting fail, StatusCode(" + statusCode + ") invalid. for url : " + url);
+			}
 
-            // result
-            bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-            StringBuilder result = new StringBuilder();
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                result.append(line);
-            }
-            String resultJson = result.toString();
+			// result
+			bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+			StringBuilder result = new StringBuilder();
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				result.append(line);
+			}
+			String resultJson = result.toString();
 
-            // parse returnT
-            try {
-                ReturnT returnT = GsonTool.fromJson(resultJson, ReturnT.class, returnTargClassOfT);
-                return returnT;
-            } catch (Exception e) {
-                logger.error("xxl-job remoting (url=" + url + ") response content invalid(" + resultJson + ").", e);
-                return new ReturnT<String>(ReturnT.FAIL_CODE, "xxl-job remoting (url=" + url + ") response content invalid(" + resultJson + ").");
-            }
+			// parse returnT
+			try {
+				return GsonTool.fromJson(resultJson, ReturnT.class, returnTargClassOfT);
+			} catch (Exception e) {
+				logger.error("xxl-job remoting (url=" + url + ") response content invalid(" + resultJson + ").", e);
+				return new ReturnT<String>(ReturnT.FAIL_CODE, "xxl-job remoting (url=" + url + ") response content invalid(" + resultJson + ").");
+			}
 
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            return new ReturnT<String>(ReturnT.FAIL_CODE, "xxl-job remoting error(" + e.getMessage() + "), for url : " + url);
-        } finally {
-            try {
-                if (bufferedReader != null) {
-                    bufferedReader.close();
-                }
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            } catch (Exception e2) {
-                logger.error(e2.getMessage(), e2);
-            }
-        }
-    }
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return new ReturnT<String>(ReturnT.FAIL_CODE, "xxl-job remoting error(" + e.getMessage() + "), for url : " + url);
+		} finally {
+			try {
+				if (bufferedReader != null) {
+					bufferedReader.close();
+				}
+				if (connection != null) {
+					connection.disconnect();
+				}
+			} catch (Exception e2) {
+				logger.error(e2.getMessage(), e2);
+			}
+		}
+	}
 
 }
