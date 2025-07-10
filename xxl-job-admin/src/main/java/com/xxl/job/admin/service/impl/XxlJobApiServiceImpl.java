75de1fdd9f5f957c8fadc46b12333e329bf6eeee
daoyang <lidaoyang@hotmail.com>
2025-07-10 16:26:12 +0800

    feat(XxlJobApiServiceImpl):为新建作业设置默认值
    - 添加 setJobInfoDefaultVal 方法，用于设置作业信息的默认值- 在 addJob 方法中调用该方法，为新建作业设置以下默认值：
      - 作者：SYSTEM
      - 执行超时时间：0
      - 失败重试次数：1
      - Glue 类型：BEAN
      - 执行器路由策略：LEAST_FREQUENTLY_USED
      - 漏火策略：DO_NOTHING
      -执行器阻塞策略：SERIAL_EXECUTION

--- xxl-job-admin/src/main/java/com/xxl/job/admin/service/impl/XxlJobApiServiceImpl.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/service/impl/XxlJobApiServiceImpl.java
@@ -2,9 +2,13 @@ package com.xxl.job.admin.service.impl;
 
 import com.xxl.job.admin.core.model.XxlJobInfo;
 import com.xxl.job.admin.core.model.XxlJobUser;
+import com.xxl.job.admin.core.route.ExecutorRouteStrategyEnum;
+import com.xxl.job.admin.core.scheduler.MisfireStrategyEnum;
 import com.xxl.job.admin.service.XxlJobApiService;
 import com.xxl.job.admin.service.XxlJobService;
 import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.enums.ExecutorBlockStrategyEnum;
+import com.xxl.job.core.glue.GlueTypeEnum;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,8 +32,21 @@ public class XxlJobApiServiceImpl implements XxlJobApiService {
 
         XxlJobUser xxlJobUser = new XxlJobUser();
         xxlJobUser.setRole(1); // 1-管理员,不需权限控制
+        // 设置默认值
+        setJobInfoDefaultVal(jobInfo);
         return xxlJobService.add(jobInfo, xxlJobUser);
     }
+
+    private void setJobInfoDefaultVal(XxlJobInfo xxlJobInfo) {
+        xxlJobInfo.setAuthor("SYSTEM");
+        xxlJobInfo.setExecutorTimeout(0); // 执行超时时间
+        xxlJobInfo.setExecutorFailRetryCount(1);// 失败重试次数
+        xxlJobInfo.setGlueType(GlueTypeEnum.BEAN.getDesc());
+        xxlJobInfo.setExecutorRouteStrategy(ExecutorRouteStrategyEnum.LEAST_FREQUENTLY_USED.name());
+        xxlJobInfo.setMisfireStrategy(MisfireStrategyEnum.DO_NOTHING.name());
+        xxlJobInfo.setExecutorBlockStrategy(ExecutorBlockStrategyEnum.SERIAL_EXECUTION.name());
+    }
+
     @Override
     public ReturnT<String> remove(int id) {
         return xxlJobService.remove(id);
