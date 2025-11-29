b48a68a07a94de08dbf7a2b196a3baa1127d584f
xuxueli <931591021@qq.com>
2025-11-29 20:43:11 +0800

    update doc

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot-ai/src/test/java/com/xxl/job/executor/test/BaseTests.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot-ai/src/test/java/com/xxl/job/executor/test/BaseTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class XxlJobExecutorExampleBootApplicationTests {
+public class BaseTests {
 
 	@Test
 	public void test() {
