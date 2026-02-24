3e49757047db1e77d18a8922b52c76d4e56a4bc0
luamas <luamas@163.com>
2026-02-24 16:14:40 +0800

    修改标题支持64位长度

--- doc/db/tables_xxl_job_pgsql.sql
+++ doc/db/tables_xxl_job_pgsql.sql
@@ -40,7 +40,7 @@ CREATE TABLE xxl_job_group
 (
     id           SERIAL PRIMARY KEY,
     app_name     VARCHAR(64)  NOT NULL,
-    title        VARCHAR(12)  NOT NULL,
+    title        VARCHAR(64)  NOT NULL,
     address_type SMALLINT     NOT NULL DEFAULT 0,
     address_list TEXT,
     update_time  TIMESTAMP
